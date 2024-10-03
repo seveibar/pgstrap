48d7bc6dbf50818bb655165226606f19412832d7
seveibar <seveibar@gmail.com>
2024-10-02 21:07:07 -0700

    support for pglite

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,101 +1,103 @@
 // pgstrap.ts
-import * as nodePgMigrateModule from "node-pg-migrate";
-import { Client } from "pg";
-import Debug from "debug";
-import path from "path";
+import * as nodePgMigrateModule from "node-pg-migrate"
+import { Client } from "pg"
+import Debug from "debug"
+import path from "path"
 import {
-	getConnectionStringFromEnv,
-	getPgConnectionFromEnv,
-} from "pg-connection-from-env";
-import * as childProcess from "child_process";
-import { Context } from "./get-project-context";
+  getConnectionStringFromEnv,
+  getPgConnectionFromEnv,
+} from "pg-connection-from-env"
+import * as childProcess from "child_process"
+import { Context } from "./get-project-context"
 
-const debug = Debug("pgstrap");
+const debug = Debug("pgstrap")
 
 export async function createMigration(name: string, ctx: Context) {
-	childProcess.execSync(
-		`npx node-pg-migrate --migration-file-language ts -m ${
-			ctx.dbDir ?? "src/db"
-		}/migrations create ${name}`,
-	);
-	console.log(`Migration ${name} created`);
+  childProcess.execSync(
+    `npx node-pg-migrate --migration-file-language ts -m ${
+      ctx.dbDir ?? "src/db"
+    }/migrations create ${name}`,
+  )
+  console.log(`Migration ${name} created`)
 }
 
 export async function migrate(
-	ctx: Context & {
-		databaseUrl?: string;
-		migrationsDir?: string;
-		client?: Client;
-	},
+  ctx: Context & {
+    databaseUrl?: string
+    migrationsDir?: string
+    client?: Client
+  },
 ) {
-	if (!ctx.migrationsDir && ctx.dbDir) {
-		ctx.migrationsDir = path.join(ctx.dbDir, "migrations");
-	}
+  if (!ctx.migrationsDir && ctx.dbDir) {
+    ctx.migrationsDir = path.join(ctx.dbDir, "migrations")
+  }
 
-	const client =
-		ctx.client ??
-		new Client(
-			ctx.databaseUrl ??
-				getConnectionStringFromEnv({
-					fallbackDefaults: {
-						database: ctx.defaultDatabase,
-					},
-				}),
-		);
+  const client =
+    ctx.client ??
+    new Client(
+      ctx.databaseUrl ??
+        getConnectionStringFromEnv({
+          fallbackDefaults: {
+            database: ctx.defaultDatabase,
+          },
+        }),
+    )
 
-	// pglite compat
-	if ("connect" in client) {
-		await client.connect();
-	}
+  // pglite compat
+  if ("connect" in client) {
+    await client.connect()
+  }
 
-	let logger =
-		debug.enabled || process.env.NODE_ENV !== "test"
-			? console
-			: {
-					...console,
-					info: () => null,
-					log: () => null,
-				};
+  let logger =
+    debug.enabled || process.env.NODE_ENV !== "test"
+      ? console
+      : {
+          ...console,
+          info: () => null,
+          log: () => null,
+        }
 
-	const runMigrations = () =>
-		// some es import bug when it's built- something with interop
-		(
-			(nodePgMigrateModule as any).default.default ??
-			nodePgMigrateModule.default
-		)({
-			dbClient: client,
-			direction: "up",
-			schema: "public",
-			createSchema: true,
-			createMigrationsSchema: true,
-			migrationsSchema: "migrations",
-			migrationsTable: "pgmigrations",
-			verbose: false,
-			dir: ctx.migrationsDir ?? path.join(ctx.cwd, "src/db/migrations"),
-			logger,
-		});
+  const runMigrations = () =>
+    // some es import bug when it's built- something with interop
+    (
+      (nodePgMigrateModule as any).default.default ??
+      nodePgMigrateModule.default
+    )({
+      dbClient: client,
+      direction: "up",
+      schema: "public",
+      createSchema: true,
+      createMigrationsSchema: true,
+      migrationsSchema: "migrations",
+      migrationsTable: "pgmigrations",
+      verbose: false,
+      dir: ctx.migrationsDir ?? path.join(ctx.cwd, "src/db/migrations"),
+      logger,
+    })
 
-	logger.info("Running migrations...");
-	try {
-		await runMigrations();
-	} catch (err: any) {
-		if (
-			err
-				.toString()
-				.includes("SyntaxError: Cannot use import statement outside a module")
-		) {
-			console.log(
-				"Couldn't load migrations due to import issue, using esbuild-register to enable import...",
-			);
-			require("esbuild-register");
-			await runMigrations();
-		} else {
-			throw err;
-		}
-	}
+  logger.info("Running migrations...")
+  try {
+    await runMigrations()
+  } catch (err: any) {
+    if (
+      err
+        .toString()
+        .includes("SyntaxError: Cannot use import statement outside a module")
+    ) {
+      console.log(
+        "Couldn't load migrations due to import issue, using esbuild-register to enable import...",
+      )
+      require("esbuild-register")
+      await runMigrations()
+    } else {
+      throw err
+    }
+  }
 
-	await client.end();
+  if ("end" in client) {
+    await client.end()
+  }
 
-	// debug only
-	// console.log("Migrations completed")
+  // debug only
+  // console.log("Migrations completed")
 }
--- test/pglite.test.ts
+++
@@ -1,7 +0,0 @@
-import { PGlite } from "@electric-sql/pglite"
-import test from "ava"
-
-test("migration of a pglite db works", async (t) => {
-  const client = new PGlite()
-  console.log(await client.query("SELECT 1"))
-})
